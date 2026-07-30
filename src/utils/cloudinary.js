d2e45dfd70d1aef3d0e40a572e3bf7c0f29f0e47
Siddhant Rana <siddhantrana.sr.official@gmail.com>
2026-07-31 00:48:16 +0530

    Established cloudinary credentials in the env & created a utility service name cloudinary.js to upload a file from server's local storage to cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINDARY_API_SECRET
+});
+
+import { v2 as cloudinary } from 'cloudinary';
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null
+    // upload the file on Cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto"
+    })
+    // file has been uploaded successfully
+    console.log("File is uploaded on Cloudinary ", response.url)
+    return response
+  } catch (error) {
+    fs.unlinkSync(localFilePath) // Remove the locally saved temporary file as the upload got failed
+    return null
+  }
+}
+
+
+export { uploadOnCloudinary }
+
+
+
