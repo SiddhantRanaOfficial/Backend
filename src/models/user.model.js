c0d7240621ad5330c4d54374968b094f9519d12b
Siddhant Rana <siddhantrana.sr.official@gmail.com>
2026-07-30 15:34:07 +0530

    Installed bcrypt dependencies to ensure hashing of passwords before saving to the database using mongoose hooks and password authentication using mongoose custom method & bcrypt methods.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new mongoose.SchemaType({
   username: {
@@ -43,5 +45,13 @@ const userSchema = new mongoose.SchemaType({
   }
 }, { timestamps: true })
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10)
+  next()
+})
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
 
 export const User = mongoose.model("User", userSchema)
