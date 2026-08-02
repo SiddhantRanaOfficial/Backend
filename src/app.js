7bb4ed51c0e48425e5cbb2380c662eef60fe8b68
Siddhant Rana <siddhantrana.sr.official@gmail.com>
2026-08-02 23:52:07 +0530

    Created test user route & controller & fixed some bugs (Server is running)

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
+// Registering middlewares
+
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
   credentials: true
@@ -12,7 +14,20 @@ app.use(cors({
 app.use(express.json({
   limit: "16kb"
 }))
+
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+
 app.use(express.static("public"))
+
 app.use(cookieParser())
+
+// Routes import
+
+import userRouter from './routes/user.routes.js'
+
+// Routes declaration
+
+app.use("/api/v1/users", userRouter)
+
+
 export { app }
